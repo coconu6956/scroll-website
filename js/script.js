e379193459a2a69b18036d0eef90b9469d18c83d
coconu6956 <wce0422@gmail.com>
2023-08-23 02:45:47 +0900

    done

--- js/script.js
+++ js/script.js
@@ -12,8 +12,90 @@ let t1 = gsap.timeline({
 })
 .to(container, {
   x:()=>
-  -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+  -(container.scrollWidth - document.documentElement.clientWidth - 135) + "px",
   ease: "none",
   duration: 1,
 })
-.to({}, {duration: 1 / (section.length + 1)})
+
+.to('.side-bar', {
+  x: 70,
+  opacity: 1, 
+  scrollTrigger: {
+    trigger: ".side-bar",
+    start: "center+=2500 center",
+    scrub: 2.5,
+  }
+})
+.to({}, {duration: 1 / (section.length + 1)});
+
+gsap.to('.col-1', {
+  y: -250,
+  ease: "none",
+  duration: 2,
+  scrollTrigger:{
+    trigger: ".image-gallery",
+    start: "top center",
+    end: "+=3000",
+    scrub: true,
+  }
+});
+
+gsap.to('.col-2', {
+  y: 250,
+  ease: "none",
+  duration: 2,
+  scrollTrigger:{
+    trigger: ".image-gallery",
+    start: "top center",
+    end: "+=3000",
+    scrub: true,
+  }
+});
+
+gsap.to('.col-3', {
+  y: -250,
+  ease: "none",
+  duration: 2,
+  scrollTrigger:{
+    trigger: ".image-gallery",
+    start: "top center",
+    end: "+=3000",
+    scrub: true,
+  }
+});
+
+gsap.from('.row-2',{
+  height: "0%",
+  duration: 1,
+  delay: 0.5,
+  scrollTrigger:{
+    trigger: ".section-3",
+    start: "40% center",
+    toggleActions: "play pause reverse reverse",
+  }
+});
+
+gsap.from(".row li",{
+  y:200,
+  opacity: 0,
+  delay: 2,
+  ease: "none",
+  duration: 2,
+  stagger: {
+    amout: 1
+  },
+  scrollTrigger: {
+    trigger: ".row li",
+    toggleActions: "play pause reverse reverse",
+  },
+});
+
+gsap.to('.num', {
+  x: 600,
+  delay: 0.5,
+  duration: 2,
+  scrollTrigger: {
+    trigger: '.num',
+    start: 'left right',
+  }
+})
